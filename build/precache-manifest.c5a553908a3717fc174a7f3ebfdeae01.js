d9d7ad1825705095bf337b270238fcd2b25cea71
Diwan Muhamad <63192756+ChangBrady@users.noreply.github.com>
2021-12-20 19:27:38 +0800

    Add files via upload

---
+++ build/precache-manifest.c5a553908a3717fc174a7f3ebfdeae01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ef5a29ca131e3648753bf869b0d5a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c94c60096d92cdba0f58",
+    "url": "/static/css/main.526b5212.chunk.css"
+  },
+  {
+    "revision": "00596227ee0105e52f5e",
+    "url": "/static/js/2.45aaaad2.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.45aaaad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94c60096d92cdba0f58",
+    "url": "/static/js/main.a3e1d8ca.chunk.js"
+  },
+  {
+    "revision": "8e4f0424cb592ebe3b3f",
+    "url": "/static/js/runtime-main.5c9d46ed.js"
+  }
+]);
